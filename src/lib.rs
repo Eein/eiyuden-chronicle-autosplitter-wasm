dbba6bcc1097fa218f0d3522c31e2adcac2c77ae
William Volin <me@williamvolin.com>
2024-06-02 11:47:26 -0700

    make load remover do load remover things

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![feature(type_alias_impl_trait, const_async_blocks)]
 #![no_std]
 
-use asr::{ timer::{self, TimerState}, future::next_tick, settings::Gui, Process, PointerSize, game_engine::unity::il2cpp};
+use asr::{ timer::self, future::next_tick, settings::Gui, Process, PointerSize, game_engine::unity::il2cpp};
 use asr::game_engine::unity::il2cpp::Version;
 
 asr::panic_handler!();
@@ -9,17 +9,15 @@ asr::async_main!(nightly);
 
 #[derive(Gui)]
 struct Settings {
-    /// My Setting
+    /// Load Removal
     #[default = true]
-    my_setting: bool,
+    load_removal: bool,
     // TODO: Change these settings.
 }
 
 async fn main() {
     // TODO: Set up some general state and settings.
     let mut settings = Settings::register();
-
-    asr::print_message("Hello, World!");
 
     loop {
         let process = Process::wait_attach("EiyudenChronicle.exe").await;
@@ -45,24 +43,11 @@ async fn main() {
                     };
 
                     if let Some(true) = loading_value {
-                        match timer::state() {
-                            TimerState::NotRunning => {
-                                timer::start()
-                            },
-                            TimerState::Ended=> {
-                                timer::start()
-                            },
-                            TimerState::Paused => {
-                                timer::split()
-                            },
-                            _ => ()
-                        }
-                        timer::resume_game_time()
-                    } else {
                         timer::pause_game_time()
+                    } else {
+                        timer::resume_game_time()
                     }
 
-                    // TODO: Do something on every tick.
                     next_tick().await;
                 }
             })
